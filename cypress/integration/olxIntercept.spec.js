2ed3675d7ccb2657be92b1e8bd61d40466d25b56
pwojakiewicz <pwojakiewicz@spyro-soft.com>
2022-06-08 16:20:33 +0200

    fix for two tests with intercept

--- cypress/integration/olxIntercept.spec.js
+++ cypress/integration/olxIntercept.spec.js
@@ -7,23 +7,20 @@ describe('Tests main OLX page using intercept', () => {
 
     it('Should be able to check search request', () => {
         let searchText = "truskawki"
-        cy.intercept(
-            'GET',
-            'https://www.olx.pl/api/v1/offers**'
-        ).as('SearchForItems')
+        cy.intercept('GET',`**/offers/metadata/search/**`).as('SearchForItems')
         cy.visit('https://www.olx.pl/')
         cy.get(olxLocators.acceptCookies).click()
         cy.get(olxLocators.searchInput).type(searchText)
         cy.get(olxLocators.searchButton).click()
-        cy.url().should('contain', searchText)
         cy.wait('@SearchForItems').its('request.url').should('contain', searchText)
+        cy.url().should('contain', searchText)
     })
-
+//PONIZSZE NADAL DO ZMIANY
     it('Should be able to mock no items found', () => {
         let searchText = "truskawki"
         cy.intercept(
             'GET',
-            'https://www.olx.pl/api/v1/offers**',
+            `**/offers/metadata/search/**`,
             { fixture: 'olxNoItemsFound.json'}
         )
         cy.visit('https://www.olx.pl/')
@@ -37,14 +34,14 @@ describe('Tests main OLX page using intercept', () => {
     it('Should be able to change banner text and url', () => {
         cy.intercept(
             'GET',
-            '*?country=pl&brand=olx&language=pl&type=banner&channel=desktop_we',
+            '**/onb/content**',
             { fixture: 'olxBannerSample.json' }
-        ) 
+        ).as('olxBannerResponse') 
         cy.visit('https://www.olx.pl/')
+        cy.wait('@olxBannerResponse')
         cy.get(olxLocators.acceptCookies).click()
         cy.get(olxLocators.bannerText).should('have.text', olxBannerSample[0].text)
         cy.get(olxLocators.bannerLink).should('have.text', olxBannerSample[0].ctaLabel)
-        cy.get(olxLocators.bannerLink).should('', olxBannerSample[0].ctaUrl)
+        cy.get(olxLocators.bannerLink).should('have.attr', 'href', olxBannerSample[0].ctaUrl)
     })
-
 })
--- cypress/pages/olxPages.js
+++ cypress/pages/olxPages.js
@@ -1,7 +1,7 @@
 export const olxLocators = {
     acceptCookies: '#onetrust-accept-btn-handler',
     bannerText: '.bannerWrapper #text',
-    bannerLink: 'cta-link',
+    bannerLink: '#cta-link',
     searchInput: '[data-cy="homepage_input_textsearch"]',
     searchButton: '[data-cy="homepage_button_search"]',
     singleItemCard: '[data-cy="l-card"]'
